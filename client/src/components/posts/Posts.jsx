2a6095bb2a5739cef109f8340f0632c2e7773b36
Chandni <chandnik96.40@gmail.com>
2024-07-26 13:13:34 +0530

    Add posts and follow users

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,20 +1,27 @@
 import { makeRequest } from "../../axios";
+// eslint-disable-next-line no-unused-vars
 import Post from "../post/Post";
 import "./posts.scss";
 import {useQuery } from 'react-query';
 
 const Posts = () => {
   
-    const { isLoading, error, data } = useQuery(['posts'], () =>
+    const { isLoading, error, data } = useQuery(["posts"], () =>
       makeRequest.get("/posts").then((res)=>{
         return res.data
       })
-    )
-  return <div className="posts">
-    {data.map(post=>(
-      <Post post={post} key={post.id}/>
-    ))}
-  </div>;
+    );
+    console.log(data);
+  return ( 
+  <div className="posts">
+   {error 
+    ? "Something went wrong" 
+    : isLoading 
+    ? "loading" 
+    : data.map((post)=>
+      <Post post={post} key={post.id}/>)}
+  </div>
+    );
 };
 
 export default Posts;
