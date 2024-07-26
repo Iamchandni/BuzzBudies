3d9d00072c95e53c3a01c720052f3fc5c75a9a97
Chandni <chandnik96.40@gmail.com>
2024-07-26 16:13:19 +0530

    Upload images and create post

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import commentRoute from "./routes/comments.js"
 import likeRoute from "./routes/likes.js"
 import userRoute from "./routes/users.js"
 import cors from "cors";
+import multer from "multer";
 import CookiePaser from "cookie-parser";
 import cookieParser from "cookie-parser";
 
@@ -18,6 +19,22 @@ app.use(cors({
     origin:"http://localhost:3000",
 }));
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../client/public/upload')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now()+file.originalname)
+    }
+  })
+  
+const upload = multer({ storage: storage });
+app.post("/api/upload",upload.single("file"),(req,res)=>{
+    const file=req.file;
+    res.status(200).json(file.filename);
+})
+
 app.use(express.json());
 
 app.use("/api/auth",authRoute);
